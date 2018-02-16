4d683cd8f1f3c378dd0ff75524449df94f695a83
unknown <munozfra@LECA2020.FR>
2018-02-16 18:18:57 +0100

    Include "scenarios" in input arguments

--- R/create.phylo.all.R
+++ R/create.phylo.all.R
@@ -1,8 +1,8 @@
-create.phylo.all <- function()
+create.phylo.all <- function(scenarios = "S3")
 # Used once to generate the phylogeny of all the taxa of the database
 {
   data('TreeGhatsData', package='TreeGhats', envir=environment())
   TreeGhatsData <- get("TreeGhatsData", envir=environment())
   TreeGhatsData.accept <- TreeGhatsData[(!is.na(TreeGhatsData$Status_TBGRI) | !is.na(TreeGhatsData$Status_thePlantlist)) & ((is.na(TreeGhatsData$Status_TBGRI) & TreeGhatsData$Status_thePlantlist=="Accepted") | TreeGhatsData$Status_TBGRI=="Accepted"),]
-  TreeGhatsData.phylo <- create.phylo(TreeGhatsData.accept)
+  TreeGhatsData.phylo <- create.phylo(TreeGhatsData.accept, scenarios = scenarios)
 }
