d01eceed8bd6d51b0776f9611c1cf3e8960255e4
unknown <munozfra@leca2020.fr>
2018-03-08 10:28:37 +0100

    Update field name Status_TPL

--- R/create.phylo.all.R
+++ R/create.phylo.all.R
@@ -3,6 +3,6 @@ create.phylo.all <- function(scenarios = "S3")
 {
   data('TreeGhatsData', package='TreeGhats', envir=environment())
   TreeGhatsData <- get("TreeGhatsData", envir=environment())
-  TreeGhatsData.accept <- TreeGhatsData[(!is.na(TreeGhatsData$Status_TBGRI) | !is.na(TreeGhatsData$Status_thePlantlist)) & ((is.na(TreeGhatsData$Status_TBGRI) & TreeGhatsData$Status_thePlantlist=="Accepted") | TreeGhatsData$Status_TBGRI=="Accepted"),]
+  TreeGhatsData.accept <- TreeGhatsData[(!is.na(TreeGhatsData$Status_TBGRI) | !is.na(TreeGhatsData$Status_TPL)) & ((is.na(TreeGhatsData$Status_TBGRI) & TreeGhatsData$Status_TPL=="Accepted") | TreeGhatsData$Status_TBGRI=="Accepted"),]
   TreeGhatsData.phylo <- create.phylo(TreeGhatsData.accept, scenarios = scenarios)
 }
